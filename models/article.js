af2a5e7bc11341bd9c97f419b005e4df5f65513f
petejos80 <petejos80@gmail.com>
2018-07-12 13:42:53 -0500

    Added API and components directory

--- models/article.js
+++ models/article.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-  title: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  headline: { type: String, required: true },
+  date: { type: String, required: true },
   url: { type: String, required: true }
 });
 
