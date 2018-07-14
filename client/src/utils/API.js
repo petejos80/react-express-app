8917a275479c2813ee116083d03f296983803337
petejos80 <petejos80@gmail.com>
2018-07-14 14:06:36 -0500

    env

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+require ("dotenv")
 
-const apiKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931"
+// const apiKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931"
+const apiKey = process.env.NYT_API_KEY;
 
 export default {
   
@@ -21,6 +23,7 @@ export default {
 
   // Retrives articles from NYT based on search criteria
   findArticles: function(topic, startYear, endYear) {
+    console.log(apiKey)
     return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231");
   }
 };
