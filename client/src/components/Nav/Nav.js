e491a4ffbb77668ad99bd249a7e8b232c345c51e
petejos80 <petejos80@gmail.com>
2018-08-08 18:39:05 -0500

    New York Times header change

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <Container>
     <a className="navbar-brand" href="/">
-      New York Times Scraper
+      New York Times Search
     </a>
     <a className="navbar-brand" href="/saved">
       Saved Articles
